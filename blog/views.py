3eec0d1c4b4f8a79460680857217d4add91c9a70
VillamarCarlos <cgvillamarl@gmail.com>
2021-05-12 09:00:54 -0400

    Agregando comentarios de usuarios y botones

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,10 @@ from .forms import PostForm
 
 from django.contrib.auth.decorators import login_required
 
+from .forms import PostForm, CommentForm
+
+from .models import Post, Comment
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -67,4 +71,29 @@ def post_remove(request, pk):            #Inicio de post_remove
     post.delete()
     return redirect('post_list')
 
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
 
